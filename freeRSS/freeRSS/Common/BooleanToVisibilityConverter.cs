0e09c8d45f79d36264b48eea8307d6970866ec12
Aaron <mgsweet@126.com>
2017-04-28 20:31:03 +0800

    UI 1.0

---
+++ freeRSS/freeRSS/Common/BooleanToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace freeRSS.Common
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts true values to Visibility.Visible and false values to
+        /// Visibility.Collapsed, or the reverse if the parameter is "Reverse".
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, string language) =>
+            (bool)value ^ (parameter as string ?? string.Empty).Equals("Reverse") ?
+                Visibility.Visible : Visibility.Collapsed;
+
+        /// <summary>
+        /// Converts Visibility.Visible values to true and Visibility.Collapsed 
+        /// values to false, or the reverse if the parameter is "Reverse".
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+            (Visibility)value == Visibility.Visible ^ (parameter as string ?? string.Empty).Equals("Reverse");
+    }
+
+}
---
+++ freeRSS/freeRSS/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace freeRSS
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private bool IsOpen;    //  这东西到时候改到VIEW MODEL 里面去
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            setTitleUI();
+            IsOpen = true;
+        }   
+        
+        /// <summary>
+        /// 设置自定义标题栏控件
+        /// </summary>
+        private void setTitleUI()
+        {
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            Window.Current.SetTitleBar(GridTitleBar);
+            var view = ApplicationView.GetForCurrentView();
+            view.TitleBar.ButtonBackgroundColor = Color.FromArgb(0, 0, 0, 0);
+            view.TitleBar.ButtonForegroundColor = Colors.White;
+
+            // inactive
+            view.TitleBar.InactiveBackgroundColor = Color.FromArgb(0, 0, 0, 0);
+            view.TitleBar.InactiveForegroundColor = Colors.Gray;
+
+        }
+
+        private void PaneOpenTrigger_Click(object sender, RoutedEventArgs e)
+        {
+            RootSplitView.IsPaneOpen = RootSplitView.IsPaneOpen ? false : true;
+        }
+    }
+
+   
+}
