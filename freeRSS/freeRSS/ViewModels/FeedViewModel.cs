eb86e5192fde1510279c543dcfeee2428727d100
longjj <luojj26@mail2.sysu.edu.cn>
2017-05-14 22:28:13 +0800

    实现unreadnum，articlemodel增加了Iconsourceasstring

--- freeRSS/freeRSS/ViewModels/FeedViewModel.cs
+++ freeRSS/freeRSS/ViewModels/FeedViewModel.cs
@@ -212,18 +212,14 @@ namespace freeRSS.ViewModels
 
         // 9. total Unread Num
         // 要考虑下是不是每次触发了unread都去遍历一次article看有多少文章未读，还是直接减一
-        private int _unreadNum;
+        //private int _unreadNum;
         public int UnreadNum
         {
-            set
-            {
-                SetProperty(ref _unreadNum, value);
-            }
             get
             {
-                return _unreadNum;
+                return this.Articles.ToList().Where(x => x.UnRead == true).Count();
             }
-        } 
+        }
 
         /// <summary>
         /// set the shourcut img's source async.
